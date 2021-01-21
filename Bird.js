e4fd839d5a2984cd521a8e68f1373a4720d15039
hemasri-source <76712464+hemasri-source@users.noreply.github.com>
2021-01-20 18:14:02 -0600

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,27 @@
+class Bird {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.8
+          
+      }
+      this.body = Bodies.rectangle(x, y, 50, 50, options);
+      this.width = 50;
+      this.height = 50;
+      
+      World.add(myWorld, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle= this. body. angle;
+      pos.x = mouseX
+      pos.y = mouseY
+      push ()
+      translate (pos.x, pos.y)
+      rotate (angle) 
+      rectMode(CENTER);
+      fill("green");
+      rect( 0, 0, this.width, this.height);
+      pop ()
+    
+    }
+  }
